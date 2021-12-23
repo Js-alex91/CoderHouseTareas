e9d087b20c9b4831dd343680a675474b861cde0b
Jonatan <alexisssevero@gmail.com>
2021-12-22 22:02:19 -0300

    tarea1

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//variables 
+const nombre = prompt("ingrese su nombre");
+const apellido = prompt("ingrese su apellido");
+let edad = prompt("ingresa tu edad");
+let saludo = "hola ";
+let respuesta = `${saludo} usted se llama ${nombre} ${apellido} y su edad es ${edad} años gracias.`;
+
+//calculo
+let numero1 = parseInt(prompt("ingrese un numero para sumar"));
+let numero2 = parseInt(prompt("ingrese un segundo numero para sumar"));
+let resultadoSuma = numero1 + numero2;
+
+//alert
+alert(respuesta);
+alert(resultadoSuma);
+
+//console.log
+console.log(respuesta);
+console.log(resultadoSuma);
